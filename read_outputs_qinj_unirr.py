00dd50456a93d66f121a7e203895df4c7852cdf5
LucaMenzio <46999301+LucaMenzio@users.noreply.github.com>
2024-10-08 15:08:38 +0200

    Integrated current and some path changes in the unirr script

--- read_outputs_qinj_unirr.py
+++ read_outputs_qinj_unirr.py
@@ -34,10 +34,17 @@ def extract_info(s: str):
 module_id = 43
 timestamps = ["2024-10-01-11-55-39","2024-10-01-12-07-27","2024-10-01-12-16-44","2024-10-01-12-28-02","2024-10-01-12-37-39", "2024-10-01-12-48-40", "2024-10-01-13-00-17"]  # List of timestamps
 dose = "0E14"
+fit_options = "QR+"
+current_0e14_negtemp = []
+current = current_0e14_negtemp
 
 dir_path = "../from_systest_pc/outputs/"
 path_results_qinj = "../from_systest_pc/results/" + str(module_id) + "/"
-    
+if module_id == 43:
+    outdir = "2x2UFSD4_W17_T9_-20C_unirr/"
+else:
+    outdir = "2x2UFSD4_W17_T9_-20C_6e14/"
+
 # Find corresponding voltage, temperature, and pixel information
 # Assumes the user is smart enought to provide timestamps of the same series
 all_dirs_there = [entry for entry in os.listdir(path_results_qinj) if os.path.isdir(os.path.join(path_results_qinj, entry))]
@@ -46,23 +53,26 @@ for dir in all_dirs_there:
         voltage, temperature, pixel = extract_info(dir.replace(timestamps[0], ""))
 
 # Create the output ROOT file
-outfilename = f"results_mod{str(module_id)}_{pixel}_{dose}_{temperature}.root"
+outfilename = f"{outdir}results_mod{str(module_id)}_{pixel}_{dose}_{temperature}.root"
 outFile = ROOT.TFile.Open(outfilename, 'RECREATE')
 tree = ROOT.TTree("qinj_results", "qinj_results")
 
 # Variables to store data in the TTree
 t_charge = np.zeros(1, dtype=int)
 t_width = np.zeros(1, dtype=float)
-t_width_norm = np.zeros(1, dtype=float)
+t_HM_left = np.zeros(1, dtype=float)
 t_sigma_left = np.zeros(1, dtype=float)
 t_sigma_right = np.zeros(1, dtype=float)
 t_timestamp = np.zeros(1, dtype=float)
 t_bias = np.zeros(1, dtype=int)
+if len(current) != 1:
+    t_current = np.zeros(1, dtype=int)
+    tree.Branch("current", t_current, "current/D")
 
 # Define branches
 tree.Branch("charge", t_charge, "charge/I")
 tree.Branch("width", t_width, "width/D")
-tree.Branch("width_norm", t_width_norm, "width_norm/D")
+tree.Branch("HM_lleft", t_HM_left, "HM_left/D")
 tree.Branch("sigma_left", t_sigma_left, "sigma_left/D")
 tree.Branch("sigma_right", t_sigma_right, "sigma_right/D")
 tree.Branch("timestamp", t_timestamp, "timestamp/D")
@@ -90,7 +100,7 @@ for timestamp in timestamps:
     json_files = [file for file in os.listdir(filepath) if file.endswith('.json')]
 
     # Lists to store the results for this timestamp
-    charges, width, width_norm, sigma_left, sigma_right = [], [], [], [], []
+    charges, width, width_norm, sigma_left, sigma_right, HM_left = [], [], [], [], [], []
 
     # Process each json file for this timestamp
     for i, file in enumerate(json_files):
@@ -115,8 +125,8 @@ for timestamp in timestamps:
         fit_functions_right.SetParameters((lowLim_right[i] + highLim_right[i]) / 2., 20., 8., 4.)
 
         # Perform the fits
-        graph_hits.Fit(fit_functions_left, 'QR+')
-        graph_hits.Fit(fit_functions_right, 'QR+')
+        graph_hits.Fit(fit_functions_left, fit_options)
+        graph_hits.Fit(fit_functions_right, fit_options)
 
         # Set graph properties
         graph_hits.SetMarkerStyle(20)
@@ -127,10 +137,10 @@ for timestamp in timestamps:
         x_left = fit_functions_left.GetParameter(1) - fit_functions_left.GetParameter(2) * ROOT.TMath.Sqrt(2 * ROOT.TMath.Log(2))
         x_right = fit_functions_right.GetParameter(0)
         width.append(abs(x_left - x_right))
-        width_norm.append(width[i] / charges[i])
         sigma_left.append(fit_functions_left.GetParameter(2))
         sigma_right.append(fit_functions_right.GetParameter(1))
-
+        HM_left.append(x_left)
+        
         # Add graph to the multigraph
         mg.Add(graph_hits)
         legend.AddEntry(graph_hits, f'{charges[i]} fC', 'lp')
@@ -138,11 +148,13 @@ for timestamp in timestamps:
         # Fill the TTree
         t_charge[0] = charges[i]
         t_width[0] = width[i]
-        t_width_norm[0] = width_norm[i]
+        t_HM_left[0] = HM_left[i]
         t_sigma_left[0] = sigma_left[i]
         t_sigma_right[0] = sigma_right[i]
         t_timestamp[0] = float(mktime(datetime.strptime(timestamp, "%Y-%m-%d-%H-%M-%S").timetuple()))
         t_bias[0] = int(voltage.replace("V",""))
+        if len(current) > 1:
+            t_current[0] = current[j]
         tree.Fill()
 
     # Draw the multigraph
@@ -155,7 +167,7 @@ for timestamp in timestamps:
     # Update and show the canvas
     canvas.Update()
     canvas.Draw()
-    canvas.SaveAs(f"Qinj_vs_Vth_{voltage}.png")
+    canvas.SaveAs(f"{outdir}Qinj_vs_Vth_{voltage}.png")
 
 # Write the TTree and close the file
 tree.Write()
